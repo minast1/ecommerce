1917ae00e179b56841b2cf44fea2ae42c3fd4d1f
minast1 <edmarfo2@gmail.com>
2022-10-06 15:09:17 +0000

    more styling on appbar

--- app/components/DashboardLayout.tsx
+++ app/components/DashboardLayout.tsx
@@ -1,16 +1,10 @@
 import * as React from "react";
-import { styled, type Theme, type CSSObject } from "@mui/material/styles";
 import Box from "@mui/material/Box";
-import MuiDrawer from "@mui/material/Drawer";
-import MuiAppBar, {
-  type AppBarProps as MuiAppBarProps,
-} from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import CssBaseline from "@mui/material/CssBaseline";
-import Typography from "@mui/material/Typography";
+//import Typography from "@mui/material/Typography";
 import Divider from "@mui/material/Divider";
 import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import ListItem from "@mui/material/ListItem";
@@ -20,89 +14,23 @@ import CustomNav from "./CustomNav";
 import navlist from "~/src/Nav";
 import ListItemBody from "./ListItemBody";
 import SingleLevelBody from "./SingleLevelBody";
-
-const drawerWidth = 240;
-
-const openedMixin = (theme: Theme): CSSObject => ({
-  width: drawerWidth,
-  transition: theme.transitions.create("width", {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.enteringScreen,
-  }),
-  overflowX: "hidden",
-});
-
-const closedMixin = (theme: Theme): CSSObject => ({
-  transition: theme.transitions.create("width", {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  overflowX: "hidden",
-  width: `calc(${theme.spacing(7)} + 1px)`,
-  [theme.breakpoints.up("sm")]: {
-    width: `calc(${theme.spacing(8)} + 1px)`,
-  },
-});
-
-const DrawerHeader = styled("div")(({ theme }) => ({
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "flex-end",
-  padding: theme.spacing(0, 1),
-  // necessary for content to be below app bar
-  ...theme.mixins.toolbar,
-}));
-
-interface AppBarProps extends MuiAppBarProps {
-  open?: boolean;
-}
-
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== "open",
-})<AppBarProps>(({ theme, open }) => ({
-  zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(["width", "margin"], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(["width", "margin"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
-}));
-
-const Drawer = styled(MuiDrawer, {
-  shouldForwardProp: (prop) => prop !== "open",
-})(({ theme, open }) => ({
-  width: drawerWidth,
-  flexShrink: 0,
-
-  whiteSpace: "nowrap",
-  boxSizing: "border-box",
-  ...(open && {
-    ...openedMixin(theme),
-
-    "& .MuiDrawer-paper": openedMixin(theme),
-  }),
-  ...(!open && {
-    ...closedMixin(theme),
-    "& .MuiDrawer-paper": closedMixin(theme),
-  }),
-}));
+//import MenuItem from "@mui/material/MenuItem";
+//import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import MoreIcon from "@mui/icons-material/MoreVert";
+import { Search, SearchIconWrapper, StyledInputBase } from "./Search";
+import Button from "@mui/material/Button";
+import { AppBar, Drawer, DrawerHeader } from "./AppBar";
+import { Avatar } from "@mui/material";
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  //const theme = useTheme();
   const [open, setOpen] = React.useState(false);
-  //const openDropdown = useNavStore((state) => state.open);
-  // const handleOpen = useNavStore((state) => state.handleOpen);
+
   const handleDrawerOpen = () => {
     setOpen(true);
   };
@@ -128,26 +56,52 @@ export default function DashboardLayout({
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" noWrap component="div">
-            Mini variant drawer
-          </Typography>
+
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon sx={{ color: "gray" }} />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ "aria-label": "search" }}
+            />
+          </Search>
+          <Box sx={{ flexGrow: 1 }} />
+          <Box sx={{ display: { xs: "none", md: "flex" }, gap: 1 }}>
+            <Button size="small" sx={{ px: 2, py: 0 }} color="inherit">
+              Logout
+            </Button>
+
+            <Avatar src="/avatar-1.jpg" />
+          </Box>
+          <Box sx={{ display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="show more"
+              // aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              //onClick={handleMobileMenuOpen}
+              color="inherit"
+            >
+              <MoreIcon />
+            </IconButton>
+          </Box>
         </Toolbar>
       </AppBar>
       <Drawer
         variant="permanent"
         open={open}
         sx={{
+          height: "100%",
+
           "& .MuiDrawer-paper": {
             backgroundImage: "url(/drawerbg.jpg)",
             backgroundSize: "cover",
-            //minHeight: "100vh",
-            backgroundPosition: "center center",
             "&:before": {
               position: "absolute",
               width: "100%",
-              minHeight: "100vh",
+              minHeight: open ? "120vh" : "100vh", //conditionally set it based on the contents height
               content: '""',
-              display: "block",
               background: "#000",
               opacity: "0.8",
             },
